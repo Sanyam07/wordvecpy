118b216ba3fbe65337cd74afb5fcfe98a78f4844
Shane Stephenson <stephenson.shane.a@gmail.com>
2019-06-19 22:43:33 -0400

    fixed version number

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     author_email='stephenson.shane.a@gmail.com', 
     packages=find_packages(),
     install_requires = ['numpy', 'tqdm'],
-    version='v1.0',
+    version='v1.1',
     license="All yours bro",
     description='Package for working with word vector embeddings',
     long_description_content_type='text/markdown',
     long_description=open('README.md', 'r').read(),
-    download_url = 'https://github.com/metriczulu/wordvecpy/archive/v1.0.tar.gz'
+    download_url = 'https://github.com/metriczulu/wordvecpy/archive/v1.1.tar.gz'
 )
